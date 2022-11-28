70be3fc12c1da42976981caa5688e64a4233b321
Andreu Mut <andrac275@gmail.com>
2022-11-28 15:02:01 +0100

    estructura basica componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { SearchMovieComponent } from './components/search-movie/search-movie.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MovieComponent, MoviesListComponent, SearchMovieComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
