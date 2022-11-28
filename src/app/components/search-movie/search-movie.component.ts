0d49b1244ef9cec214c35ebd1583b0a430120d68
Andreu Mut <andrac275@gmail.com>
2022-11-28 15:03:03 +0100

    estructura basica componentes

---
+++ src/app/components/search-movie/search-movie.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-movie',
+  templateUrl: './search-movie.component.html',
+  styleUrls: ['./search-movie.component.css'],
+})
+export class SearchMovieComponent {}
